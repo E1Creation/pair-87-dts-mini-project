ac6e3281d5917cfbc69870afc3b4d60c4982dd86
Syarif Nurhidayah <syarif.nurhid@gmail.com>
2022-07-22 00:23:13 +0700

    auth login

--- src/components/MovieItem.jsx
+++ src/components/MovieItem.jsx
@@ -3,44 +3,85 @@ import React from "react";
 import { imageUrl } from "../services/Movies/TMDBMoviesAPI";
 import StarRateIcon from "@mui/icons-material/StarRate";
 import { Link, useNavigate } from "react-router-dom";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth, logOut } from "../services/authentication/firebase";
 
 const MovieItem = ({ popular }) => {
+  const [user, isLoading] = useAuthState(auth);
   return (
-    <Link to={`/detail/${popular.id}`} style={{ textDecoration: "none" }}>
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          bgcolor: "background.paper",
-          maxWidth: 200,
-          m: 2,
-          borderRadius: 2,
-          overflow: "hidden",
-        }}
-      >
-        <Box
-          component="img"
-          sx={{
-            width: 200,
-            maxWidth: { xs: 100, md: 200 },
-          }}
-          src={imageUrl + popular.poster_path}
-          alt={popular.original_title}
-        />
-        <Box sx={{ p: "5px" }}>
+    <Box>
+      {user ? (
+        <Link to={`/detail/${popular.id}`} style={{ textDecoration: "none" }}>
           <Box
             sx={{
               display: "flex",
-              flexDirection: "row",
+              flexDirection: "column",
+              alignItems: "center",
+              bgcolor: "background.paper",
+              maxWidth: 200,
+              m: 2,
+              borderRadius: 2,
+              overflow: "hidden",
             }}
           >
-            <StarRateIcon />
-            {popular.vote_average}
+            <Box
+              component="img"
+              sx={{
+                width: 200,
+                maxWidth: { xs: 100, md: 200 },
+              }}
+              src={imageUrl + popular.poster_path}
+              alt={popular.original_title}
+            />
+            <Box sx={{ p: "5px" }}>
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "row",
+                }}
+              >
+                <StarRateIcon />
+                {popular.vote_average}
+              </Box>
+            </Box>
+          </Box>
+        </Link>
+      ) : (
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            bgcolor: "background.paper",
+            maxWidth: 200,
+            m: 2,
+            borderRadius: 2,
+            overflow: "hidden",
+          }}
+        >
+          <Box
+            component="img"
+            sx={{
+              width: 200,
+              maxWidth: { xs: 100, md: 200 },
+            }}
+            src={imageUrl + popular.poster_path}
+            alt={popular.original_title}
+          />
+          <Box sx={{ p: "5px" }}>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+              }}
+            >
+              <StarRateIcon />
+              {popular.vote_average}
+            </Box>
           </Box>
         </Box>
-      </Box>
-    </Link>
+      )}
+    </Box>
   );
 };
 
